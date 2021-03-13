b08c4a0dcf6a0730466c727b826fbf9be8c343ab
eethomas14 <ethomas14@luc.edu>
2021-03-13 12:58:50 -0600

    Initial commit

---
+++ src/main/java/com/springpractice/covid19tracker/controllers/HomeController.java
@@ -0,0 +1,26 @@
+package com.springpractice.covid19tracker.controllers;
+
+import com.springpractice.covid19tracker.models.LocationStats;
+import com.springpractice.covid19tracker.services.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    CoronaVirusDataService coronaVirusDataService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        int totalCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        model.addAttribute("locationStats");
+        return "home";
+
+    }
+}
