c19fb8ffe2f6941982e385dd559d7fb4b189d812
eethomas14 <ethomas14@luc.edu>
2021-03-13 15:24:24 -0600

    added a column for changes since the last day and added the new total new cases since previous day to the alert. also added some bootstrap styling to the header which includes a reference to the JHU data source

--- src/main/java/com/springpractice/covid19tracker/models/LocationStats.java
+++ src/main/java/com/springpractice/covid19tracker/models/LocationStats.java
@@ -4,6 +4,7 @@ public class LocationStats {
     private String state;
     private String country;
     private int latestTotalCases;
+    private int diffFromPrevDay;
 
     public String getState() {
         return state;
@@ -36,5 +37,13 @@ public class LocationStats {
                 ", country='" + country + '\'' +
                 ", latestTotalCases=" + latestTotalCases +
                 '}';
+    }
+
+    public int getDiffFromPrevDay() {
+        return diffFromPrevDay;
+    }
+
+    public void setDiffFromPrevDay(int diffFromPrevDay) {
+        this.diffFromPrevDay = diffFromPrevDay;
     }
 }
--- src/main/java/com/springpractice/covid19tracker/services/CoronaVirusDataService.java
+++ src/main/java/com/springpractice/covid19tracker/services/CoronaVirusDataService.java
@@ -42,7 +42,10 @@ public class CoronaVirusDataService {
             LocationStats locationStat = new LocationStats();
             locationStat.setState(record.get("Province/State"));
             locationStat.setCountry(record.get("Country/Region"));
-            locationStat.setLatestTotalCases(Integer.parseInt(record.get(record.size()-1)));
+            int latestCases = Integer.parseInt(record.get(record.size()-1));
+            int previousDayCases = Integer.parseInt(record.get(record.size()-2));
+            locationStat.setLatestTotalCases(latestCases);
+            locationStat.setDiffFromPrevDay(latestCases-previousDayCases);
             newStats.add(locationStat);
         }
     this.allStats = newStats;
